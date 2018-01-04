b5fba40a9af56c57234ad365d4824497d3221c16
Tejas Shroff <tejasshroff24@gmail.com>
2018-01-03 21:29:19 -0500

    Added SteamVR
    Added Steam VR and primitive controller testing

---
+++ Trajectory-Prototype-0.1/Assets/Game/Scripts/HandController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HandController : MonoBehaviour {
+
+    private SteamVR_TrackedObject trackedObj;
+    private SteamVR_Controller.Device device;
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        device = SteamVR_Controller.Input((int)trackedObj.index);
+
+        if (device.GetPressDown(SteamVR_Controller.ButtonMask.Trigger))
+        {
+            print("Get trigger down");
+        }
+
+        if (device.GetPressUp(SteamVR_Controller.ButtonMask.Trigger))
+        {
+            print("Get trigger up");
+        }
+
+    }
+}
