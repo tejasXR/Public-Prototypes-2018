7c98b2e1250e4831fe116cf1d79ef91a0bd2d3ed
Tejas Shroff <tejasshroff24@gmail.com>
2018-01-17 08:38:59 -0500

    Added randomized upgrade spawns

--- Underworld-VR-Prototype-0.3/Assets/Game/Scripts/UpgradeManager.cs
+++ Underworld-VR-Prototype-0.3/Assets/Game/Scripts/UpgradeManager.cs
@@ -6,14 +6,24 @@ public class UpgradeManager : MonoBehaviour {
 
     public GameManager gameManager;
 
-    public GameObject upgrade1;
-    public GameObject upgrade2;
-    public GameObject upgrade3;
+    // A list of all the total upgrades between the different rarities
+    public GameObject[] blueUpgrades; //Common upgrades
+    public GameObject[] greenUpgrades; //Uncommon upgrades
+    public GameObject[] redUpgrades; //Rare upgrades
+    public GameObject[] silverUpgrades; //Epic upgrades
+    public GameObject[] goldUpgrades; //Legendary upgrades
 
-    public Transform upgradeStation1;
-    public Transform upgradeStation2;
-    public Transform upgradeStation3;
+    // A list of the 3 upgrdaes that will be chosen
+    public GameObject[] upgrades;
 
+    //public GameObject upgrade2;
+    //public GameObject upgrade3;
+
+    public Transform[] upgradeStationSlots; //Transforms for where the chosen upgrades need to go
+    //public Transform upgradeStation2;
+    //public Transform upgradeStation3;
+
+    public bool upgradesRandomized = false;
     public bool upgradeSelected = false;
 
     private float upgradeBufferTimer = 1.5f; //the buffer time between after the player upgrades and the wave starts
@@ -22,11 +32,7 @@ public class UpgradeManager : MonoBehaviour {
     // Use this for initialization
     void Start () {
         gameManager = GameObject.Find("GameManager").GetComponent<GameManager>();
-        //upgrade1.SetActive(false);
-        //upgrade2.SetActive(false);
-        //upgrade3.SetActive(false);
-
-        //UpgradeReturn();
+       
     }
 
     // Update is called once per frame
@@ -34,46 +40,79 @@ public class UpgradeManager : MonoBehaviour {
 
         if (gameManager.upgradeActive)
         {
-            //print("UpgradeManager");
-
+            if (!upgradesRandomized)
+            {
+                RandomizeUpgrades();
+                //UpgradesNull();
+                UpgradesCreated();
+                upgradeBufferTimer = 1.5f;
+                upgradesRandomized = true;
+            }
             UpgradePlacement();
-            
         }
 
         if (upgradeSelected)
         {
             UpgradeReturn();
-            //gameManager.StartNewWave();
-            //upgradeSelected = false;
         }
 		
 	}
 
+    void RandomizeUpgrades()
+    {
+        // For each of the 3 upgrade choices, assign a random probability to each of the classes of upgrades to choose from
+        for (int i = 0; i < 3; i++)
+        {
+            var randomChance = Random.Range(0, 100);
+
+            if (randomChance < 50) { upgrades[i] = blueUpgrades[Random.Range(0, blueUpgrades.Length)]; }
+            if (randomChance > 50 && 75 > randomChance) { upgrades[i] = greenUpgrades[Random.Range(0, greenUpgrades.Length)]; }
+            if (randomChance > 75 && 90 > randomChance) { upgrades[i] = redUpgrades[Random.Range(0, redUpgrades.Length)]; }
+            if (randomChance > 90 && 95 > randomChance) { upgrades[i] = silverUpgrades[Random.Range(0, silverUpgrades.Length)]; }
+            if (randomChance > 95 && 99 > randomChance) { upgrades[i] = goldUpgrades[Random.Range(0, goldUpgrades.Length)]; }
+            print(randomChance);
+        }
+    }
+
+    void UpgradesCreated()
+    {
+        for (int i = 0; i < 3; i++)
+        {            
+            upgrades[i] = Instantiate(upgrades[i], transform.position, transform.rotation) as GameObject;
+        }
+    }
+
     void UpgradePlacement()
     {
-        upgrade1.transform.position = Vector3.Lerp(upgrade1.transform.position, upgradeStation1.transform.position, Time.deltaTime * 2f);
-        upgrade1.transform.rotation = Quaternion.Slerp(upgrade1.transform.rotation, upgradeStation1.transform.rotation, Time.deltaTime);
-
-        upgrade2.transform.position = Vector3.Lerp(upgrade2.transform.position, upgradeStation2.transform.position, Time.deltaTime * 2f);
-        upgrade2.transform.rotation = Quaternion.Slerp(upgrade2.transform.rotation, upgradeStation2.transform.rotation, Time.deltaTime);
-
-        upgrade3.transform.position = Vector3.Lerp(upgrade3.transform.position, upgradeStation3.transform.position, Time.deltaTime * 2f);
-        upgrade3.transform.rotation = Quaternion.Slerp(upgrade3.transform.rotation, upgradeStation3.transform.rotation, Time.deltaTime);
+        for (int i = 0; i < 3; i++)
+        {
+            upgrades[i].transform.position = Vector3.Lerp(upgrades[i].transform.position, upgradeStationSlots[i].transform.position, Time.deltaTime * 2f);
+            upgrades[i].transform.rotation = Quaternion.Slerp(upgrades[i].transform.rotation, upgradeStationSlots[i].transform.rotation, Time.deltaTime);
+            //print("upgrade moving");
+        }
     }
 
     void UpgradeReturn()
     {
-        upgrade1.transform.position = Vector3.Lerp(upgrade1.transform.position, new Vector3(0, -15, 0), Time.deltaTime * 2f);
-        upgrade2.transform.position = Vector3.Lerp(upgrade2.transform.position, new Vector3(0, -15, 0), Time.deltaTime * 2f);
-        upgrade3.transform.position = Vector3.Lerp(upgrade3.transform.position, new Vector3(0, -15, 0), Time.deltaTime * 2f);
+        foreach (GameObject item in upgrades)
+        {
+            item.transform.position = Vector3.Lerp(item.transform.position, new Vector3(0, -15, 0), Time.deltaTime * 2f);
+        }
 
         upgradeBufferTimer -= Time.deltaTime;
         if (upgradeBufferTimer <= 0)
         {
             gameManager.upgradeActive = false;
             upgradeSelected = false;
+            upgradesRandomized = false;
         }
+    }
 
-
+    void UpgradesNull()
+    {
+        for (int i = 0; i < 3; i++)
+        {
+            upgrades[i] = null;
+        }
     }
 }
