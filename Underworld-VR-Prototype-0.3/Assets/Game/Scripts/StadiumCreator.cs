2b9ed11d5fa5f0457817b4d0c672b46d5504971d
Tejas Shroff <tejasshroff24@gmail.com>
2018-01-17 19:20:54 -0500

    Major Improvements in setting

---
+++ Underworld-VR-Prototype-0.3/Assets/Game/Scripts/StadiumCreator.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StadiumCreator : MonoBehaviour {
+
+    public float radius;
+    public float degrees;
+    private float x;
+    private float z;
+
+
+	// Use this for initialization
+	void Start () {
+
+        x = radius * Mathf.Sin(degrees);
+        z = radius * Mathf.Cos(degrees);
+
+		//transform.position = new Vector3()
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
