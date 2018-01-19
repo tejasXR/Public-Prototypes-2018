236e66ab4eff91614bd119428ee268da434c24b1
Tejas Shroff <tejasshroff24@gmail.com>
2018-01-19 06:55:44 -0500

    Light map creation

--- Underworld-VR-Prototype-0.3/Assets/Game/Scripts/StadiumLightMove.cs
+++ Underworld-VR-Prototype-0.3/Assets/Game/Scripts/StadiumLightMove.cs
@@ -14,7 +14,7 @@ public class StadiumLightMove : MonoBehaviour {
     public float zAngleMax;
 
     public float moveFrequency;
-    private float moveFrequencyTimer;
+    public float moveFrequencyTimer;
 
     public float moveSpeed;
 
@@ -23,21 +23,28 @@ public class StadiumLightMove : MonoBehaviour {
     // Use this for initialization
     void Start () {
         moveFrequencyTimer = moveFrequency;
+        RandomAngle();
+        moveFrequencyTimer = Random.Range(1, moveFrequencyTimer);
 	}
 	
 	// Update is called once per frame
 	void Update () {
-        transform.rotation = Quaternion.SlerpUnclamped(transform.rotation, rotation, Time.deltaTime * moveSpeed);
+        transform.rotation = Quaternion.Lerp(transform.rotation, rotation, Time.deltaTime * moveSpeed);
 
 
         moveFrequencyTimer -= Time.deltaTime;
+
         if (moveFrequencyTimer <= 0)
         {
             RandomAngle();
-            moveFrequencyTimer = moveFrequency;
+            //print("changing");
+
+            moveFrequencyTimer = Random.Range(1, moveFrequency);
+            print("changing");
+
         }
 
-	}
+    }
 
     void RandomAngle()
     {
