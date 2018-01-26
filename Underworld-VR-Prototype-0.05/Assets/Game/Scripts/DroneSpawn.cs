40ad8f57eedb595c426277e591d09257f2857c5e
Tejas Shroff <tejasshroff24@gmail.com>
2018-01-26 12:02:19 -0500

    Better enemy spawning system

--- Underworld-VR-Prototype-0.05/Assets/Game/Scripts/DroneSpawn.cs
+++ Underworld-VR-Prototype-0.05/Assets/Game/Scripts/DroneSpawn.cs
@@ -7,9 +7,13 @@ public class DroneSpawn : MonoBehaviour {
     public float enemySpawnTimer;
     private float spawnBufferTimer = 1.5f;
     public GameObject enemySpawnObj;
-
+    public Transform spawnPoint;
     private GameObject playerController;
 
+    public GameObject mesh;
+    private Renderer triangleRend;
+    public float triangleAlpha = 0f;
+    public float alphaSpeed;
     private bool hasSpawnedEnemy;
 
 
@@ -24,6 +28,8 @@ public class DroneSpawn : MonoBehaviour {
         //Quaternion rotation = Quaternion.LookRotation(looking);
         //transform.rotation = Quaternion.LookRotation(looking, Vector3.up);
         transform.LookAt(playerController.transform.position);
+
+        triangleRend = mesh.GetComponent<Renderer>();
 	}
 	
 	// Update is called once per frame
@@ -32,18 +38,28 @@ public class DroneSpawn : MonoBehaviour {
         enemySpawnTimer -= Time.deltaTime;
         if (enemySpawnTimer <= 0 && !hasSpawnedEnemy)
         {
-            Instantiate(enemySpawnObj, transform.position, enemySpawnObj.transform.rotation);
+            Instantiate(enemySpawnObj, spawnPoint.transform.position, enemySpawnObj.transform.rotation);
             hasSpawnedEnemy = true;
         }
         if (hasSpawnedEnemy)
         {
             spawnBufferTimer -= Time.deltaTime;
+
             if (spawnBufferTimer <= 0)
             {
                 Destroy(this.gameObject);
             }
         }
-        
+        if (!hasSpawnedEnemy)
+        {
+            triangleAlpha = Mathf.Lerp(triangleAlpha, 0.4f, Time.deltaTime * alphaSpeed);
+        } else
+        {
+            triangleAlpha = Mathf.Lerp(triangleAlpha, 0f, Time.deltaTime * alphaSpeed);
+        }
+
+        triangleRend.material.SetFloat("_MKGlowTexStrength", triangleAlpha);
+
 
     }
 }
