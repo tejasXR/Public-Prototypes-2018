f52e724ae8a482b239c50fc48f251e2aca496be1
Tejas Shroff <tejasshroff24@gmail.com>
2018-01-31 16:36:42 -0500

    Added blue upgrades

--- Underworld-VR-Prototype-0.07/Assets/Game/Scripts/Upgrades.cs
+++ Underworld-VR-Prototype-0.07/Assets/Game/Scripts/Upgrades.cs
@@ -14,6 +14,7 @@ public class Upgrades : MonoBehaviour {
     // Player Health Upgrades
     public float addPlayerHealth;
     public float addPlayerHealthMaxMultiplier;
+    public float addPlayerHealthRegenMultiplier;
 
     // Player Attack Upgrades
     public float addBulletFireRateMultiplier;
@@ -68,6 +69,7 @@ public class Upgrades : MonoBehaviour {
         // Player Health Effects
         playerController.playerHealthMaxMultiplier += addPlayerHealthMaxMultiplier;
         playerController.playerHealth += addPlayerHealth;
+        playerController.playerHealthRegenMultiplier += addPlayerHealthRegenMultiplier;
 
 
         // Player Defense Effects
