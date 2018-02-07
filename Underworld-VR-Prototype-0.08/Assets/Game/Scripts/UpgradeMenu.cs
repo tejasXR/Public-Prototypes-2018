047a1e986533df815ed57fbf94481a3b899747ed
Tejas Shroff <tejasshroff24@gmail.com>
2018-02-06 22:00:48 -0500

    First pass at integrated menu system

--- Underworld-VR-Prototype-0.08/Assets/Game/Scripts/UpgradeMenu.cs
+++ Underworld-VR-Prototype-0.08/Assets/Game/Scripts/UpgradeMenu.cs
@@ -12,44 +12,141 @@ public class UpgradeMenu : MonoBehaviour {
 
     public float angleFromCenter; //gets the angle of the finger on the touchpad in relation to the center of the touchpad (0,0)
 
-    public int currentMenuItem; //current menu item
-    private int oldMenuItem; //old menu item
+    public int currentMainMenuItem; //current menu item for the main menu
+    private int oldMainMenuItem; //old menu item for the main menu
 
-    private bool weaponsSelected;
+    public int currentAttackUpgradeItem;
+    public int oldAttackUpgradeItem;
+
+    public int currentDefenseUpgradeItem;
+    public int oldDefenseUpgradeItem;
+
+    public int currentWeaponUpgradeItem;
+    public int oldWeaponUpgradeItem;
+
+    private bool upgradeSelected;
 
     private Vector2 touchpad;
     public GameObject upgradeMenu;
 
+    public GameObject cursor;
+
     public bool upgradeMenuOpen;
+    public bool upgradeMenuActive;
+
+    public bool attackUpgradesOpen;
+    public bool attackUpgradesActive;
+
+    public bool weaponUpgradesOpen;
+    public bool weaponUpgradesActive;
+
+    public bool defenseUpgradesOpen;
+    public bool defenseUpgradesActive;
+
+    public GameObject[] attackUpgrades;
+    public GameObject[] defenseUpgrades;
+    public GameObject[] weaponUpgrades;
+
+    
 
 
     // Use this for initialization
     void Start () {
+
+        MenuReset();
+        
 		
 	}
-	
-	// Update is called once per frame
-	void FixedUpdate () {
+
+    void Update()
+    {
+        if (upgradeMenuOpen)
+        {
+            OpenUpgradeMenu();
+        }
+
+        if (attackUpgradesOpen)
+        {
+            OpenAttackUpgradeMenu();
+        }
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
 
         device = SteamVR_Controller.Input((int)trackedObj.index);
 
         if (device.GetPressDown(SteamVR_Controller.ButtonMask.Touchpad))
         {
-            OpenUpgradeMenu();
+            upgradeMenuOpen = true;
         }
 
         if (device.GetPressDown(SteamVR_Controller.ButtonMask.Touchpad) && upgradeMenuOpen)
         {
 
         }
-		
-	}
+
+        
+
+
+    }
 
 
     void OpenUpgradeMenu()
     {
         upgradeMenu.SetActive(true);
-        upgradeMenuOpen = true;
+
+        touchpad.x = device.GetAxis(Valve.VR.EVRButtonId.k_EButton_SteamVR_Touchpad).x;
+        touchpad.y = device.GetAxis(Valve.VR.EVRButtonId.k_EButton_SteamVR_Touchpad).y;
+
+        Vector2 fromVector2 = new Vector2(0, 1);
+        Vector2 toVector2 = touchpad;
+
+        angleFromCenter = Vector2.Angle(fromVector2, toVector2);
+        Vector3 cross = Vector3.Cross(fromVector2, toVector2);
+        if (cross.z > 0)
+        {
+            angleFromCenter = 360 - angleFromCenter;
+        }
+
+        if (Mathf.Abs(touchpad.x) > .3f || Mathf.Abs(touchpad.y) > .3f && upgradeMenuActive && !attackUpgradesActive && !defenseUpgradesActive && !weaponUpgradesActive)
+        {
+            //map angle from center to specific buttons;
+            if (340 < angleFromCenter || angleFromCenter <= 20)
+            {
+                currentMainMenuItem = 0; //Weapon Upgrades
+                print("Weapon upgrades");
+
+
+            }
+            else if (250 < angleFromCenter && angleFromCenter <= 290)
+            {
+                currentMainMenuItem = 1; //Attack Upgrades
+                print("attack upgrades");
+
+                if (device.GetPressDown(SteamVR_Controller.ButtonMask.Touchpad))
+                {
+                    attackUpgradesOpen = true;
+                    attackUpgradesActive = true;
+                    upgradeMenuActive = false;
+                }
+
+            }
+            else if (70 < angleFromCenter && angleFromCenter <= 110)
+            {
+                currentMainMenuItem = 2; //Defense Upgrades
+                print("defense upgrades");
+
+            }
+        }
+        
+
+
+
+
+
+        cursor.transform.localPosition = touchpad;
     }
 
     void UpgradeSelected()
@@ -57,10 +154,76 @@ public class UpgradeMenu : MonoBehaviour {
 
     }
 
-    void MenuRest()
+    void MenuReset()
     {
         upgradeMenu.SetActive(false);
         upgradeMenuOpen = false;
+
+        foreach(GameObject upgrade in attackUpgrades)
+        {
+            upgrade.SetActive(false);
+        }
+
+        foreach (GameObject upgrade in defenseUpgrades)
+        {
+            upgrade.SetActive(false);
+        }
+
+        foreach (GameObject upgrade in weaponUpgrades)
+        {
+            upgrade.SetActive(false);
+        }
+    }
+
+    void OpenAttackUpgradeMenu()
+    {
+        foreach (GameObject upgrade in attackUpgrades)
+        {
+            upgrade.SetActive(true);
+        }
+
+        if (Mathf.Abs(touchpad.x) > .3f || Mathf.Abs(touchpad.y) > .3f && attackUpgradesActive && !weaponUpgradesActive && !defenseUpgradesActive)
+        {
+            if (349 < angleFromCenter || angleFromCenter <= 11)
+            {
+                currentMainMenuItem = 0; //Weapon Upgrades
+                print("attack 1");
+            }
+            else if (304 < angleFromCenter && angleFromCenter <= 326)
+            {
+                currentMainMenuItem = 1; //Attack Upgrades
+                print("attack 2");
+            }
+            else if (259 < angleFromCenter && angleFromCenter <= 281)
+            {
+                currentMainMenuItem = 2; //Attack Upgrades
+                print("attack 3");
+            }
+            else if (214 < angleFromCenter && angleFromCenter <= 236)
+            {
+                currentMainMenuItem = 3; //Defense Upgrades
+                print("attack 4");
+
+            }
+            else if (169 < angleFromCenter && angleFromCenter <= 191)
+            {
+                currentMainMenuItem = 4; //Defense Upgrades
+                print("attack 5");
+            }
+        }
+
+        
+
+    }
+
+    void OpenDefenseUpgradeMenu()
+    {
+
+    }
+
+    void OpenWeaponUpgradeMenu()
+    {
+
     }
 
 
