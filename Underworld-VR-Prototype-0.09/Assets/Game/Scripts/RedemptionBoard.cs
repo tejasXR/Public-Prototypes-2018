e5d5f4d63a3f881440aa13e63d547ba6c24bff1a
Tejas Shroff <tejasshroff24@gmail.com>
2018-02-09 17:05:16 -0500

    more changes to redemption board

--- Underworld-VR-Prototype-0.09/Assets/Game/Scripts/RedemptionBoard.cs
+++ Underworld-VR-Prototype-0.09/Assets/Game/Scripts/RedemptionBoard.cs
@@ -4,13 +4,78 @@ using UnityEngine;
 
 public class RedemptionBoard : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
+    private GameManager gameManager;
+
+    public GameObject[] triangleStadiums;
+    public GameObject triangleCenter;
+    public bool countDown;
+
+
+    // Use this for initialization
+    void Start () {
+
+        gameManager = GameObject.Find("GameManager").GetComponent<GameManager>();
 		
 	}
-	
-	// Update is called once per frame
-	void Update () {
+
+    private void OnEnable()
+    {
+        triangleCenter.SetActive(false);
+
+        for (int i = 0; i < triangleStadiums.Length; i++)
+        {
+            triangleStadiums[i].SetActive(false);
+        }
+
+
+        StartCoroutine(RedemptionBoardEnable());
+    }
+
+    // Update is called once per frame
+    void Update () {
+
+        if (countDown)
+        {
+            StartCoroutine(RedemptionBoardCountDown());
+            countDown = false;
+        }
+
 		
 	}
+
+    IEnumerator RedemptionBoardEnable()
+    {
+
+        for(int i = 0; i <triangleStadiums.Length; i++)
+        {
+            triangleStadiums[i].SetActive(true);
+            yield return new WaitForSeconds(.05f);
+        }
+
+        triangleCenter.SetActive(false);
+        yield return new WaitForSeconds(.01f);
+        triangleCenter.SetActive(true);
+        yield return new WaitForSeconds(.01f);
+        triangleCenter.SetActive(false);
+        yield return new WaitForSeconds(.05f);
+        triangleCenter.SetActive(true);
+        yield return new WaitForSeconds(.05f);
+        triangleCenter.SetActive(false);
+        yield return new WaitForSeconds(.1f);
+        triangleCenter.SetActive(true);
+        yield return new WaitForSeconds(.1f);
+
+
+    }
+
+    IEnumerator RedemptionBoardCountDown()
+    {
+        for (int i = 9; i > -1; i--)
+        {
+            triangleStadiums[i].SetActive(false);
+            yield return new WaitForSeconds(gameManager.redemptionMeterMax / 10);
+        }
+
+        triangleCenter.SetActive(false);
+    }
 }
