b5ae79038599b00a7eccc64e5b475fa294a5a75e
Tejas Shroff <tejasshroff24@gmail.com>
2018-02-14 17:21:49 -0500

    earned bullet text changes

--- Underworld-VR-Prototype-0.11/Assets/Game/Scripts/EarnedBulletText.cs
+++ Underworld-VR-Prototype-0.11/Assets/Game/Scripts/EarnedBulletText.cs
@@ -33,39 +33,42 @@ public class EarnedBulletText : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
         //transform.position = Vector3.Lerp(transform.position, moveDirection, Time.deltaTime);
-        //Vector3.MoveTowards(transform.position, playerController.transform.position, Time.deltaTime * 2f); 
+        //Vector3.MoveTowards(transform.position, playerController.transform.position, Time.deltaTime * 2f);
+
+        transform.LookAt(playerEye.transform.position);
+        transform.position = Vector3.Lerp(transform.position, transform.forward, Time.deltaTime * .01f);
 	}
 
     IEnumerator EarnedBulletSpawn()
     {
         moveDirection = playerEye.transform.position - transform.position;
 
-        moveDirection += new Vector3(Random.Range(-1f, 1f), Random.Range(-1f, 1f), Random.Range(0f, 1f));
+        //moveDirection += new Vector3(Random.Range(-1f, 1f), Random.Range(-1f, 1f), Random.Range(0f, 1f));
 
         Ray ray = new Ray(transform.position, moveDirection);
 
-        yield return new WaitForSeconds(.5f);
+        yield return new WaitForSeconds(.25f);
 
-        textOutline1.transform.position = transform.position;
+        textOutline1.transform.position = ray.GetPoint(0f);
         textOutline1.SetActive(true);
         yield return new WaitForSeconds(.1f);
 
 
-        /*textOutline2.transform.position = ray.GetPoint(.5f);
+        textOutline2.transform.position = ray.GetPoint(.3f);
         textOutline2.SetActive(true);
-        yield return new WaitForSeconds(.1f);*/
+        yield return new WaitForSeconds(.1f);
 
 
-        textSolid.transform.position = ray.GetPoint(1f);
+        textSolid.transform.position = ray.GetPoint(.6f);
         textSolid.SetActive(true);
-        yield return new WaitForSeconds(1.25f);
+        yield return new WaitForSeconds(1.5f);
 
 
 
 
         textOutline1.SetActive(false);
-        //yield return new WaitForSeconds(.1f);
-        //textOutline2.SetActive(false);
+        yield return new WaitForSeconds(.1f);
+        textOutline2.SetActive(false);
         yield return new WaitForSeconds(.1f);
 
         textSolid.SetActive(false);
