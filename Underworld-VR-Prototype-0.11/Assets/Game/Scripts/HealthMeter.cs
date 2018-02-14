2d059c3dac83333b40bd3e71f450abfb45954fc7
Tejas Shroff <tejasshroff24@gmail.com>
2018-02-14 09:08:00 -0500

    UI for saber sword

--- Underworld-VR-Prototype-0.11/Assets/Game/Scripts/HealthMeter.cs
+++ Underworld-VR-Prototype-0.11/Assets/Game/Scripts/HealthMeter.cs
@@ -13,18 +13,34 @@ public class HealthMeter : MonoBehaviour {
     public float meterXCurrent;
     public float meterXOriginal;
 
+    public float meterZCurrent;
+    public float meterZOriginal;
+
+    public bool isOnSaberSword;
+
+
 	// Use this for initialization
 	void Start () {
 
         playerController = GameObject.Find("PlayerController").GetComponent<Player>();
         scaleOriginal = transform.localScale.x;
         meterXOriginal = transform.localPosition.x;
-	}
+
+        if (isOnSaberSword)
+        {
+            meterZOriginal = transform.localPosition.z;
+        }
+    }
 
     private void OnEnable()
     {
         scaleCurrent = 0;
         meterXCurrent = meterXOriginal - scaleOriginal / 2;
+
+        if (isOnSaberSword)
+        {
+            meterZCurrent = meterZOriginal - scaleOriginal / 2;
+        }
     }
 
     // Update is called once per frame
@@ -33,9 +49,20 @@ public class HealthMeter : MonoBehaviour {
         healthSmoothPercent = Mathf.Lerp(healthSmoothPercent, playerController.playerHealth / (playerController.playerHealthMax * playerController.playerHealthMaxMultiplier), Time.deltaTime * 5f);
 
         scaleCurrent = Mathf.Lerp(scaleCurrent, scaleOriginal * healthSmoothPercent, Time.deltaTime * 3f);
-        meterXCurrent = Mathf.Lerp(meterXCurrent, (meterXOriginal - scaleOriginal/2) +  ((scaleOriginal/2) * healthSmoothPercent), Time.deltaTime * 3f);
 
-        transform.localScale = new Vector3(scaleCurrent, transform.localScale.y, transform.localScale.z);
-        transform.localPosition = new Vector3(meterXCurrent, transform.localPosition.y, transform.localPosition.z);
+        if (isOnSaberSword)
+        {
+            meterZCurrent = Mathf.Lerp(meterZCurrent, (meterZOriginal - scaleOriginal / 2) + ((scaleOriginal / 2) * healthSmoothPercent), Time.deltaTime * 3f);
+            transform.localScale = new Vector3(scaleCurrent, transform.localScale.y, transform.localScale.z);
+            transform.localPosition = new Vector3(transform.localPosition.x, transform.localPosition.y, meterZCurrent);
+        } else
+        {
+            meterXCurrent = Mathf.Lerp(meterXCurrent, (meterXOriginal - scaleOriginal / 2) + ((scaleOriginal / 2) * healthSmoothPercent), Time.deltaTime * 3f);
+            transform.localScale = new Vector3(scaleCurrent, transform.localScale.y, transform.localScale.z);
+            transform.localPosition = new Vector3(meterXCurrent, transform.localPosition.y, transform.localPosition.z);
+        }
+
+        //transform.localScale = new Vector3(scaleCurrent, transform.localScale.y, transform.localScale.z);
+        //transform.localPosition = new Vector3(meterXCurrent, transform.localPosition.y, transform.localPosition.z);
 	}
 }
