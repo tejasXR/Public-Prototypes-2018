9d0c6dbfd53eb143be1ff8c4d67b9f4d6f62eb42
Tejas Shroff <tejasshroff24@gmail.com>
2018-02-14 17:46:06 -0500

    more earned bullet text changes

--- Underworld-VR-Prototype-0.11/Assets/Game/Scripts/EarnedBulletText.cs
+++ Underworld-VR-Prototype-0.11/Assets/Game/Scripts/EarnedBulletText.cs
@@ -12,6 +12,10 @@ public class EarnedBulletText : MonoBehaviour {
     public GameObject textSolid;
     public TextMeshPro[] texts;
     public float bulletNumber;
+    public float countSpeed = 85f;
+
+    public bool adjust;
+    public float bulletSmoothCount;
 
     public Vector3 moveDirection;
 
@@ -20,10 +24,7 @@ public class EarnedBulletText : MonoBehaviour {
 	void Start () {
         playerEye = GameObject.FindGameObjectWithTag("Player");
 
-        foreach(TextMeshPro text in texts)
-        {
-            text.text = "" + bulletNumber.ToString();
-        }
+       
 
         textOutline1.SetActive(false);
         textSolid.SetActive(false);
@@ -32,12 +33,42 @@ public class EarnedBulletText : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
+
+        if (adjust)
+        {
+            if (bulletSmoothCount <= bulletNumber)
+            {
+                bulletSmoothCount += Time.unscaledDeltaTime * countSpeed;
+            }
+            else
+            {
+                bulletSmoothCount -= Time.unscaledDeltaTime * countSpeed;
+            }
+        }
+
+        if (Mathf.Abs(bulletSmoothCount - bulletNumber) > .5f)
+        {
+            adjust = true;
+        }
+        else
+        {
+            adjust = false;
+        }
+
         //transform.position = Vector3.Lerp(transform.position, moveDirection, Time.deltaTime);
         //Vector3.MoveTowards(transform.position, playerController.transform.position, Time.deltaTime * 2f);
 
         transform.LookAt(playerEye.transform.position);
-        transform.position = Vector3.Lerp(transform.position, transform.forward, Time.deltaTime * .01f);
-	}
+        //transform.position = Vector3.Lerp(transform.position, transform.forward, Time.deltaTime * .01f);
+
+        transform.position = Vector3.Lerp(transform.position, playerEye.transform.position, Time.deltaTime * .01f);
+
+        foreach (TextMeshPro text in texts)
+        {
+            text.text = "" + bulletNumber.ToString();
+        }
+
+    }
 
     IEnumerator EarnedBulletSpawn()
     {
@@ -49,29 +80,38 @@ public class EarnedBulletText : MonoBehaviour {
 
         yield return new WaitForSeconds(.25f);
 
-        textOutline1.transform.position = ray.GetPoint(0f);
-        textOutline1.SetActive(true);
-        yield return new WaitForSeconds(.1f);
-
-
-        textOutline2.transform.position = ray.GetPoint(.3f);
-        textOutline2.SetActive(true);
-        yield return new WaitForSeconds(.1f);
-
-
-        textSolid.transform.position = ray.GetPoint(.6f);
+        //textOutline1.transform.position = ray.GetPoint(0f);
+        //textOutline1.SetActive(true);
+        //yield return new WaitForSeconds(.1f);
+        //textOutline1.SetActive(false);
+        //yield return new WaitForSeconds(.01f);
+        //textOutline2.transform.position = ray.GetPoint(.3f);
+        //textOutline2.SetActive(true);
+        //yield return new WaitForSeconds(.1f);
+        //textOutline2.SetActive(false);
+        //textSolid.transform.position = ray.GetPoint(.6f);
         textSolid.SetActive(true);
-        yield return new WaitForSeconds(1.5f);
+        yield return new WaitForSeconds(2f);
 
-
-
-
-        textOutline1.SetActive(false);
+        textSolid.SetActive(false);
         yield return new WaitForSeconds(.1f);
-        textOutline2.SetActive(false);
+        textSolid.SetActive(true);
         yield return new WaitForSeconds(.1f);
 
         textSolid.SetActive(false);
+        yield return new WaitForSeconds(.1f);
+        textSolid.SetActive(true);
+        yield return new WaitForSeconds(.1f);
+
+        textSolid.SetActive(false);
+
+
+        //textOutline1.SetActive(false);
+        //yield return new WaitForSeconds(.1f);
+        //textOutline2.SetActive(false);
+        //yield return new WaitForSeconds(.1f);
+
+        //textSolid.SetActive(false);
 
         Destroy(this.gameObject);
 
