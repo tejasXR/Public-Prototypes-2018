7695c52b4a5b01798db5d1c0b4fd5064d7ce4fb5
Tejas Shroff <tejasshroff24@gmail.com>
2018-02-15 11:53:36 -0500

    First pass at tutorials

--- Underworld-VR-Prototype-0.11/Assets/Game/Scripts/TutorialManager.cs
+++ Underworld-VR-Prototype-0.11/Assets/Game/Scripts/TutorialManager.cs
@@ -4,7 +4,9 @@ using UnityEngine;
 
 public class TutorialManager : MonoBehaviour {
 
-    public bool tutorialStart;
+    public bool inTutorial;
+
+    public TutorialStartUI tutorialStartUI;
 
 
 	// Use this for initialization
