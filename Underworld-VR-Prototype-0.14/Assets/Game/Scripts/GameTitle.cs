0b99bb43a373162fe3e4e8363e0533a135b47ee0
Tejas Shroff <tejasshroff24@gmail.com>
2018-02-27 12:24:20 -0500

    Game title + major music improvements

---
+++ Underworld-VR-Prototype-0.14/Assets/Game/Scripts/GameTitle.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameTitle : MonoBehaviour {
+
+    private GameManager gameManager;
+
+    public float delay;
+
+	// Use this for initialization
+	void Start () {
+        gameManager = GameObject.Find("GameManager").GetComponent<GameManager>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
