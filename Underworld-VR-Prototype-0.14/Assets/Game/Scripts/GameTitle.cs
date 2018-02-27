a8b20591c9501027d30102e1ab27591d60a485ee
Tejas Shroff <tejasshroff24@gmail.com>
2018-02-27 16:17:33 -0500

    major music improvemnets, more fixes to game title

--- Underworld-VR-Prototype-0.14/Assets/Game/Scripts/GameTitle.cs
+++ Underworld-VR-Prototype-0.14/Assets/Game/Scripts/GameTitle.cs
@@ -8,13 +8,45 @@ public class GameTitle : MonoBehaviour {
 
     public float delay;
 
+    public GameObject[] titles;
+
 	// Use this for initialization
 	void Start () {
         gameManager = GameObject.Find("GameManager").GetComponent<GameManager>();
+        foreach(GameObject title in titles)
+        {
+            title.SetActive(false);
+        }
+
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+        if (gameManager.gameStart)
+        {
+            delay -= Time.deltaTime;
+        }
+
+        if (delay <= 0)
+        {
+            StartCoroutine(ShowTitle());
+        }
+
 	}
+
+    IEnumerator ShowTitle()
+    {
+        titles[0].SetActive(true);
+        yield return new WaitForSeconds(.1f);
+
+        titles[1].SetActive(true);
+        yield return new WaitForSeconds(.1f);
+
+        titles[2].SetActive(true);
+        yield return new WaitForSeconds(.1f);
+
+        this.gameObject.SetActive(false);
+
+       
+    }
 }
